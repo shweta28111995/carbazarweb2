38104993dffa007a18edbaf8481e79b277d40302
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-08-31 15:31:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http');
-
-const api = require('./server/routes/api');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname, '/dist/carbazarapp'));
+app.use(express.static(__dirname + '/dist/carbazarapp'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, '/dist/carbazarapp/index.html'));
+
+res.sendFile(path.join(__dirname+'/dist/carbazarapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
