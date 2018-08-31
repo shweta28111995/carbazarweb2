45cd78e48370cc8054a97cfbfa9babd6ea6a9995
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-08-31 15:23:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
+
+const api = require('./server/routes/api');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'dist'));
+app.use(express.static(__dirname, 'dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'dist/index.html'));
+res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
