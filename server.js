ac5690cefcc6f9d7b0bd8cde99be2bb4e29ebfe6
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-08-31 15:24:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname, 'dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, 'dist/index.html'));
+res.sendFile(path.join(__dirname, 'dist/carbazarapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
