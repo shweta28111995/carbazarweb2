872031b24fe36950ece972180b3d9367ff8867a5
shweta28111995 <42740114+shweta28111995@users.noreply.github.com>
2018-09-03 09:33:11 +0530

    Update register.component.ts

--- src/app/pages/register.component.ts
+++ src/app/pages/register.component.ts
@@ -18,13 +18,7 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.resetForm();
-    this.spinner.show();
-
-    setTimeout(() => {
-        /** spinner ends after 5 seconds */
-        this.spinner.hide();
-    }, 5000);
-  }
+   
 
 
   resetForm(form?: NgForm) {
@@ -40,12 +34,12 @@ export class RegisterComponent implements OnInit {
   }
 
   OnSubmit(): void {
+    this.spinner.show();
     this.userService.registerUser(this.user)
     .subscribe((res: any) => {
-      
+      this.spinner.hide();
       debugger;
        if (res.status === 'Success') {
-  
         this.router.navigate(['signupsuccess']);
         this.toastr.success('Registered Successfully!');
         return false;
